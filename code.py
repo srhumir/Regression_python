d0ae805c4b1749457d9a3a4c8bea9d949cfa9a33
srhumir <srh.umir@gmail.com>
2016-10-03 20:16:27 +0200

    feature selection

--- code.py
+++ code.py
@@ -37,6 +37,7 @@ known_quan = data_known.iloc[:,quanind]
 known_qual = data_known.iloc[:,qualind]
 
 import sklearn.preprocessing as pp
+import math
 
 # getting correlation between variables
 # corMatrix <- cor(known_quan)
@@ -62,15 +63,59 @@ X_test_std = std.transform(X_test)
 
 ## Feature selection
 import sklearn.ensemble as en
+import sklearn.pipeline as pip
+rf_pip = pip.Pipeline([('std', pp.StandardScaler()),
+                ('rf', en.RandomForestRegressor(n_estimators=500, 
+                                                random_state=0, 
+                                                n_jobs=-2))])
 feat_lables = data_known_dummy.columns
+
 rf = en.RandomForestRegressor(n_estimators=500,
 ...                           random_state=0,
 ...                           n_jobs=-2)
-rf.fit(X_train, target_train)
+rf.fit(X_train_std, target_train)
+pred = rf.predict(X_val_std)
 imps = rf.feature_importances_
 indices = np.argsort(imps)[::-1]
-for f in range(X_train.shape[1]):
-    print("%2d) %-*s %f" % (f + 1, 30,feat_lables[f],imps[indices[f]]))
+cumsum = np.cumsum(imps[indices])
+for f in range(X_train_std.shape[1]):
+    print("%2d) %-*s %f %g" % (f + 1, 30,feat_lables[indices[f]],imps[indices[f]], cumsum[f]))
+
+def RMSE(prediction, reference):
+    SME = (prediction - reference).apply(np.square).sum()/prediction.size
+    RSME = math.sqrt(SME)
+    # ME <- sum(abs(prediction-reference))/length(prediction)
+    return(RSME)
+# threshod to choose cummulative importance
+threshold = .79
+thress = np.array(range(73,99))/100
+number_vars=[]
+train_err = []
+val_err=[]
+for threshold in thress:
+    choose = np.where(cumsum >= threshold)[0][0]
+    impvars = indices[:choose]
+    rf.fit(X_train_std[:, impvars], target_train)
+    number_vars.append(choose+1)
+    train_err.append(RMSE(rf.predict(X_train_std[:, impvars]), target_train))
+    val_err.append(RMSE(rf.predict(X_val_std[:, impvars]), target_val))
+    print("Threshod: " + str(threshold)+ ". " + str(choose+1) + " variables where chosen")
+    print("Train RMSE: " + str(train_err[-1]))
+    print ("Validation RMSE: " + str(val_err[-1]))
+
+import matplotlib.pyplot as plt
+plt.plot(thress, train_err,
+         color='blue', marker='o',
+         label='Training error')
+plt.plot(thress, val_err,
+         color='green', marker='s',
+         label='Validation error')
+plt.grid()
+plt.xlabel('Number of vraibles in the model')
+plt.ylabel('Root mean square erro')
+plt.legend(loc=7)
+plt.show()
 
 
-    print(str(f+1) + ')'+ str(feat_lables[f]) +'     '+ str(imps[indices[f]]))
+plt.hist(target)
+plt.show
